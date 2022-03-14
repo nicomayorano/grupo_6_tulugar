031fbbd370cf4327602a89c0d93496b034b16ce3
nicomayorano <nicolasmayorano@gmail.com>
2022-03-14 18:28:52 -0300

    fix: statics import path agregue normalizer y nodemon comence a implementar el index.html

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('public'));
+app.use(express.static(path.resolve(__dirname, 'public/')));
 
 const files = fs.readdirSync('./src/views/');
 for (let i = 0; i < files.length; i += 1) {
