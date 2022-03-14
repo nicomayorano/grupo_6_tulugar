1188c5f1b8dd2b1cd58ce842ce20a830a780e861
nicomayorano <nicolasmayorano@gmail.com>
2022-03-14 18:00:16 -0300

    estructura mvc implementada

---
+++ src/app.js
@@ -0,0 +1,19 @@
+// Imports
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+// Instances and constants
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+const files = fs.readdirSync('./src/views/');
+for (let i = 0; i < files.length; i += 1) {
+  app.get(`/${files[i] === 'index.html' ? '' : files[i].replace('.html', '')}`, (req, res) => {
+    res.sendFile(path.resolve(__dirname, `views/${files[i]}`));
+  });
+}
+// eslint-disable-next-line no-console
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
