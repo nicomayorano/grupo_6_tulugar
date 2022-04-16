795efba8ac534455f0e729ed6835a0132ef8bce7
nicomayorano <nicolasmayorano@gmail.com>
2022-04-16 12:08:12 -0300

    importacion y seteo como middleware dinamica de routers

--- src/app.js
+++ src/app.js
@@ -1,12 +1,7 @@
 // Native modules
 const express = require('express');
 const path = require('path');
-
-// Routers imports
-const mainRouter = require('./routes/mainRoutes');
-const userRouter = require('./routes/userRoutes');
-const productRouter = require('./routes/productRoutes');
-const adminRouter = require('./routes/adminRoutes');
+const fs = require('fs');
 
 // Instances and constants
 const app = express();
@@ -16,12 +11,15 @@ const PORT = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.set('views', path.resolve(__dirname, 'views'));
 
+// Dynamic routers import and setting as middleware
+const routers = fs.readdirSync('./src/routes/');
+for (let i = 0; i < routers.length; i += 1) {
+  // eslint-disable-next-line global-require, import/no-dynamic-require
+  app.use(`/${routers[i] === 'mainRoutes.js' ? '' : routers[i].replace('Routes.js', '')}`, require(`./routes/${routers[i]}`));
+}
+
 // Middlewares
 app.use(express.static(path.resolve(process.cwd(), 'src', 'public')));
-app.use('/', mainRouter);
-app.use('/products', productRouter);
-app.use('/users', userRouter);
-app.use('/admin', adminRouter);
 
 // eslint-disable-next-line no-console
 app.listen(PORT, () => console.log(`Running on port ${PORT}`));
