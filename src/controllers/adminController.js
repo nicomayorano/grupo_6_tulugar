00857de98ff692843ae7c28d1a08793734550bdc
JuanPabloRiglos <juan.riglos@hotmail.com>
2022-04-24 21:14:23 -0300

    funcion edit, vista, andando, y envio de formulario, terminado, pendientes testearlo, lo linkee con products.json

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,13 +1,51 @@
+const fs = require("fs");
+const path = require("path");
+
+const propiedadesFilePath = path.join(__dirname, '../data/products.json');
+const propiedades = JSON.parse(fs.readFileSync(propiedadesFilePath, 'utf-8'));
+
 const adminController = {
   index: (req, res) => {
     res.render('admin/dashboard.ejs'); // En un futuro /admin/dashboard.ejs?
   },
+
+  // ///////////////////////////////////// edicion /////////////////////////
   editForm: (req, res) => {
-    res.render('admin/edit-form.ejs');
+    const idParaEditar = Number(req.params.id);
+    const propiedadParaEditar = propiedades.find((p) => p.id === idParaEditar);
+    console.log(propiedadParaEditar)
+    res.render('admin/edit-form.ejs', { propiedad: propiedadParaEditar });
   },
   edit: (req, res) => {
-    res.render('index.ejs'); // TO DO
+    const buscado = Number(req.params.id);
+    const propiedaEditar = propiedades.find((p) => p.id === buscado);
+
+    let image = "imagen-default-tulugar.png";
+
+    if (req.file) {
+      image = req.file.filename;
+    }
+
+    propiedaEditar = {
+      id: buscado,
+      ...req.body,
+      image,
+    }
+    const propiedadesUpdate = propiedades.map((p) => {
+      if (p.id === propiedaEditar.id) {
+        return ( p ={...propiedaEditar});
+      }
+      return p;
+    });
+    fs.writeFileSync(
+      propiedadesFilePath,
+      JSON.stringify(propiedadesUpdate),
+      'utf-8');
+
+    res.render('index.ejs'); 
   },
+/////////////////////////////////////////////////TERMINA EDICION///////////////
+
   newForm: (req, res) => {
     res.render('admin/new.ejs');
   },
--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,12 +1,28 @@
 const { Router } = require('express');
+const multer = require('multer');
+const path = require('path');
 const adminController = require('../controllers/adminController');
 
 const adminRouter = new Router();
+// =================================================== MULTER ========
+const storage = multer.diskStorage({
+destination: (req, file, cb) =>{
+    let folder = path.join(__dirname, '../../public/propietiesImages');
+    cb(null, folder); 
+},
+filename:(req, file, cb) => {
+    const fileName = Date.now() + '-' + file.originalname;
+    cb(null, fileName);
+},
+});
+
+const upload = multer({ storage });
+// ------------------------------------------------------fin bloque multer-------
 
 adminRouter.post('/', adminController.index);
 
 adminRouter.get('/edit/:id', adminController.editForm);
-adminRouter.put('/edit/:id', adminController.edit);
+adminRouter.put('/edit/:id', upload.any("image"), adminController.edit);
 
 adminRouter.get('/new', adminController.newForm);
 adminRouter.post('/new', adminController.new);
