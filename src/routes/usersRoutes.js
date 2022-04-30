75c9c4ed49d07bed29836be7c3ec9f4dc56df524
Veronica Garcia <veroagarcia90@gmail.com>
2022-04-30 11:36:07 -0300

    haciendo algo con lo de informacion para ir completando huecos

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -6,7 +6,9 @@ const userRouter = new Router();
 userRouter.get('/', userController.dashboard);
 userRouter.get('/register', userController.registerForm);
 userRouter.get('/login', userController.loginForm);
+userRouter.get('/info', userController.info);
 userRouter.post('/login', userController.login);
 userRouter.post('/register', userController.register);
+
 
 module.exports = userRouter;
