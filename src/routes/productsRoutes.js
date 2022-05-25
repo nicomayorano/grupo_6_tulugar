22be12498879c0f4aa13a0344a6bb119a201dd85
Veronica Garcia <veroagarcia90@gmail.com>
2022-05-25 17:09:31 -0300

    rutas permitidas dependiendo si estas o no logueado, listo

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -2,13 +2,14 @@ const { Router } = require('express');
 const multer = require('../middlewares/multerProducts');
 const productController = require('../controllers/productController');
 const productValidation = require('../middlewares/productValidation');
+const noLogueadoMiddlewareP = require('../middlewares/noLogueadoMiddlewareP');
 
 const productRouter = new Router();
 
 productRouter.get('/', productController.index);
-productRouter.get('/search', productController.search);
-productRouter.get('/new', productController.newForm);
-productRouter.get('/:id', productController.detail);
+productRouter.get('/search', noLogueadoMiddlewareP, productController.search);
+productRouter.get('/new', noLogueadoMiddlewareP, productController.newForm);
+productRouter.get('/:id', noLogueadoMiddlewareP, productController.detail);
 productRouter.post('/', multer.array('image', 6), productValidation, productController.new);
 productRouter.get('/cart/:id', productController.cart);
 productRouter.get('/:id/edit', productController.editForm);
