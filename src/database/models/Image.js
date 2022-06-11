2676952348a7ba6042738718db7a77c6729c57be
JuanPabloRiglos <juan.riglos@hotmail.com>
2022-06-11 13:17:42 -0300

    arreglo identacion

--- src/database/models/Image.js
+++ src/database/models/Image.js
@@ -1,58 +1,53 @@
-module.exports=(sequelize, dataType) =>{
-    let alias= 'Images';
-    let cols ={
-        product_id: {
-            type: dataType.INTEGER.UNSIGNED,
-            allowNull: false
-        },
-        image1:{
-            type: dataType.STRING(255),
-            defaultValue:'default.jpg'
-        },
-        image2:{
-            type: dataType.STRING(255),
-        },
-        image3:{
-            type: dataType.STRING(255),
-        },
-        image4:{
-            type: dataType.STRING(255),
-        },
-        image5:{
-            type: dataType.STRING(255),
-        },
-        updated_at:{
-            type: dataType.DATE
-        },
-        fk_products_idx:{
-            type: dataType.INTEGER,
-            foreignkey: true
-        }
-    };
-    let config = {
-        tableName:"images",
-        timestamps: false
-    };
+module.exports = (sequelize, dataType) => {
+  const alias = 'Images';
+  const cols = {
+    product_id: {
+      type: dataType.INTEGER.UNSIGNED,
+      allowNull: false,
+    },
+    image1: {
+      type: dataType.STRING(255),
+      defaultValue: 'default.jpg',
+    },
+    image2: {
+      type: dataType.STRING(255),
+    },
+    image3: {
+      type: dataType.STRING(255),
+    },
+    image4: {
+      type: dataType.STRING(255),
+    },
+    image5: {
+      type: dataType.STRING(255),
+    },
+    updated_at: {
+      type: dataType.DATE,
+    },
+    fk_products_idx: {
+      type: dataType.INTEGER,
+      foreignkey: true,
+    },
+  };
+  const config = {
+    tableName: 'images',
+    timestamps: false,
+  };
 
-    const Images = sequelize.define(alias, cols, config);
+  const Images = sequelize.define(alias, cols, config);
 
-    
-        Images.associate = function(models){
-        Images.belongsTo(models.Products, {
-            as: "products",
-            foreignkey: "id"
-        });
-    }
+  Images.associate = function (models) {
+    Images.belongsTo(models.Products, {
+      as: 'products',
+      foreignkey: 'id',
+    });
+  };
 
+  return Images;
+};
 
-    return Images
-}
-
-
-
-//INDEX `fk_images_idx` (`product_id` ASC) VISIBLE,
-//CONSTRAINT `fk_images_pid`
-  //REFERENCES `tulugar`.`products` (`id`)
-  //ON DELETE CASCADE
+// INDEX `fk_images_idx` (`product_id` ASC) VISIBLE,
+// CONSTRAINT `fk_images_pid`
+// REFERENCES `tulugar`.`products` (`id`)
+// ON DELETE CASCADE
 //  ON UPDATE NO ACTION)
-
--- src/database/models/Product.js
+++ src/database/models/Product.js
@@ -1,63 +1,60 @@
-module.exports=(sequelize, dataType) =>{
-    let alias= 'Products';
-    let cols ={
-        id: {
-            type: dataType.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        max_guests:{
-            type: dataType.TINYINT.UNSIGNED,
-             allowNull: false
-        },
-        price:{
-            type: dataType.TINYINT.UNSIGNED,
-            allowNull: false
-        },
-        description:{
-            type: dataType.STRING,
-        },
-        province:{
-            type: dataType.STRING(45),
-        },
-        city:{
-            type: dataType.STRING(100),
-        },
-        address:{
-            type: dataType.STRING,
-        },
-        type:{
-            type: dataType.STRING(45),
-        },
-        created_at:{
-            type: dataType.DATE
-        },
-        updated_at:{
-            type: dataType.DATE
-        } 
-    };
-    let config = {
-        tableName:"products",
-        //timestamps: false
-    };
+module.exports = (sequelize, dataType) => {
+  const alias = 'Products';
+  const cols = {
+    id: {
+      type: dataType.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    max_guests: {
+      type: dataType.TINYINT.UNSIGNED,
+      allowNull: false,
+    },
+    price: {
+      type: dataType.TINYINT.UNSIGNED,
+      allowNull: false,
+    },
+    description: {
+      type: dataType.STRING,
+    },
+    province: {
+      type: dataType.STRING(45),
+    },
+    city: {
+      type: dataType.STRING(100),
+    },
+    address: {
+      type: dataType.STRING,
+    },
+    type: {
+      type: dataType.STRING(45),
+    },
+    created_at: {
+      type: dataType.DATE,
+    },
+    updated_at: {
+      type: dataType.DATE,
+    },
+  };
+  const config = {
+    tableName: 'products',
+    // timestamps: false
+  };
 
-    const Product = sequelize.define(alias, cols, config)
+  const Product = sequelize.define(alias, cols, config);
 
-    Product.associate = function(models){
-        Product.hasMany(models.Amenities, {
-            as: "imenities",
-            foreignkey: "amenities_id",
-            timestamps: false
-        });
-    
-        Product.belongsToMany(models.Images, {
-            as: "images",
-            foreignkey: "images_id"
-        });
-    
-    };
+  Product.associate = function (models) {
+    Product.hasMany(models.Amenities, {
+      as: 'imenities',
+      foreignkey: 'product_id',
+      timestamps: false,
+    });
 
+    Product.belongsToMany(models.Images, {
+      as: 'images',
+      foreignkey: 'product_id',
+    });
+  };
 
-
-    return Product
-}
+  return Product;
+};
