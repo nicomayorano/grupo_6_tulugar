ffa12414783780ee3bef9e029b358bec44da1f98
manuelAngel0909 <archivosesup@hotmail.com>
2022-06-28 23:58:48 -0300

    cambios editar usuario

--- src/routes/users.js
+++ src/routes/users.js
@@ -12,13 +12,13 @@ const userRouter = new Router();
 userRouter.get('/', userController.dashboard);
 userRouter.get('/login', onlyGuests, userController.loginForm);
 userRouter.get('/register', onlyGuests, userController.registerForm);
-userRouter.get('/edit', authRequired, userController.editForm);
+userRouter.get('/:id/edit', authRequired, userController.editForm);
 userRouter.get('/logout', authRequired, userController.logout);
 userRouter.get('/detail', userController.detail);
 userRouter.get('/info', userController.info);
 userRouter.get('/traveler', userController.traveler);
 userRouter.post('/login', loginValidation, userController.login);
 userRouter.post('/register', multer('usersImages', { size: 2_000_000, files: 1 }), registerValidation, imageValidation, userController.register);
-userRouter.patch('/edit', userController.edit);
+userRouter.post('/:id', multer('usersImages', { size: 2_000_000, files: 1 }), imageValidation, userController.edit);
 
 module.exports = userRouter;
